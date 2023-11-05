944a65953cf402a22f225a68dc846f8b4417110e
abhisoni7 <asoni2520419@gmail.com>
2023-11-05 17:59:03 -0500

    Build new issue page

--- issue-tracker/app/layout.tsx
+++ issue-tracker/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Theme>
           <Navbar />
-          <main>{children}</main>
+          <main className="pt-2 pl-5">{children}</main>
         </Theme>
       </body>
     </html>
